2e466cc124a2beea41ba127bf41d3ec673e8f3ef
hraja5 <hraja@scu.edu>
2025-03-04 23:41:36 -0800

    Add Skills and About Sections

--- harshini-portfolio/src/App.js
+++ harshini-portfolio/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-
+import { Skills } from "./components/Skills";
+import { About } from "./components/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <About />
+      <Skills />
     </div>
   );
 }
